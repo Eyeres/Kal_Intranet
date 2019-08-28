4146190a17172f0db102d791e7d976870ee54556
Eyeres <nagasatish.arigela@kalinformatics.com>
2019-08-28 08:26:02 +0530

    Auto-generated commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,20 @@
+/*
+function initChart() {
+    var chart = new OrgChart(document.getElementById('kalchart'), {
+        enableSearch: false,
+        template: "ula",
+        layout: OrgChart.tree,
+        scaleInitial: 0.75,
+        nodeBinding: {
+            field_0: "name",
+            field_1: "designation",
+            img_0: "pic"
+        },
+        nodes: [
+            { id: 1, name: "Kalyan Vempaty", designation: "CEO", pic: "http://insidekal.com/wpapi/wp-content/themes/inside/images/kalyanv.jpg" },
+            { id: 2, name: "Vamsi Krishna Gadi", designation: "BDM", pic: "http://insidekal.com/wpapi/wp-content/themes/inside/images/vamsig.jpg" },
+            { id: 3, pid: 1, spids: [2], name: "Srinivas Rao Kapparati", designation: "IT", pic: "http://insidekal.com/wpapi/wp-content/themes/inside/images/srinik.jpg" }
+        ]
+    });
+}
+*/
